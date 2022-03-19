56809477fac894f4706c53f8a24f8361771738c1
Emily Foley <efoley0823@gmail.com>
2022-03-19 00:55:10 -0400

    March 19, 2022 12:55 AM

--- exercise-app/src/components/LogTask/index.js
+++ exercise-app/src/components/LogTask/index.js
@@ -11,14 +11,7 @@ class LogTask extends React.Component {
     render() {
         return (
             <div>
-                {/* displaying the name of the exercise with a template literal  */}
-                <h1>{this.props.name}</h1>
-                {/* displaying the number of reps next to the Reps text */}
-                <p>Reps: {this.state.count}</p>
-                {/* when the button is clicked, add one to the rep count */}
-                <button onClick={() => this.setState({ count: this.state.count + 5 })}>Complete Rep</button>
-                {/* When the reset button is clicked, set the rep count to 0 */}
-                <button onClick={() => this.setState({ count: 0 })}>Reset</button>
+                <TextInput/>
             </div>
         )
     }
