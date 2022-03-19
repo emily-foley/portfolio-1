042d447ede63f647e7462c486652917663fef9b7
Emily Foley <efoley0823@gmail.com>
2022-03-19 01:22:47 -0400

    March 19, 2022 1:22 AM

--- exercise-app/src/components/LogTask/index.js
+++ exercise-app/src/components/LogTask/index.js
@@ -1,3 +1,4 @@
+//https://www.youtube.com/watch?v=PrkxUTEbZmI
 
 import React from 'react';
 
@@ -6,6 +7,7 @@ class LogTask extends React.Component {
         super()
     
         this.state = { Num1: 0, Num2: 0 }
+        this.state={Result:0};
     }
 
     Sum=()=>
@@ -14,7 +16,7 @@ class LogTask extends React.Component {
         var N2=parseInt(this.state.Num2);
 
         var R = N1 + N2;
-        alert(R);
+        this.setState(Result = R);
     }
 
     render() {
@@ -22,7 +24,8 @@ class LogTask extends React.Component {
             <div>
                 <input placeholder="Num1" onChangeText={Num1=>this.setState({Num1})}/>
                 <input placeholder="Num2" onChangeText={Num2=>this.setState({Num2})}/>
-                <button title="Sum" onClick={this.Sum}>Calculate</button>
+                <button onClick={this.Sum}>Calculate</button>
+                <input value={this.state.Result}/>
             </div>
         );
     }
