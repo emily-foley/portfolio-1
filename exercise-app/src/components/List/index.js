5eba3c1531215f3dcbbab19ef921c66f02629320
Emily Foley <efoley0823@gmail.com>
2022-03-21 20:41:41 -0400

    March 21, 2022 8:41 PM

--- exercise-app/src/components/List/index.js
+++ exercise-app/src/components/List/index.js
@@ -1,96 +1,4 @@
-// import React from 'react'
-
-// let listItems = [
-//     {
-//        id: 1,
-//        selected: false, 
-//        description: "Baby Food"
-//     },
-//     {
-//        id: 2,
-//        selected: false, 
-//        description: "Bacon"
-//     },
-//     {
-//        id: 3,
-//        selected: false, 
-//        description: "Bread"
-//     },
-//     {
-//        id: 4,
-//        selected: false, 
-//        description: "Butter"
-//     },
-//     {
-//        id: 5,
-//        selected: false, 
-//        description: "Cereal"
-//     },
-//     {
-//        id: 6,
-//        selected: false, 
-//        description: "Cheese"
-//     },
-//     {
-//        id: 7,
-//        selected: false, 
-//        description: "Cookies"
-//     },
-//     {
-//        id: 8,
-//        selected: false, 
-//        description: "Coffee"
-//     },
-//     {
-//        id: 9,
-//        selected: false, 
-//        description: "Cream"
-//     },
-//  ]
-//  export default class List extends React.Component {
-//     constructor(props) {
-//        super(props)
-//        let filterRef = React.createRef()
-//        this.state = {listItems, curId: listItems.length+1, filterRef, filtered: false}
-//     }
-//     pickedItem = (itemId) => {
-//        this.setState((prevState) => {
-//           let prevList = [...prevState.listItems]
-//           let itemIndex = prevList.findIndex((item) => item.id == itemId)
-//           prevList[itemIndex] = {...prevList[itemIndex], selected: !prevList[itemIndex].selected,}
-//           return {listItems: [...prevList]}
-//        })
-//     }
-//     render() {
-//        let listItems = this.state.listItems
-//        if (this.state.filtered) {
-//         listItems = listItems.filter(item=>!item.selected)
-//        }         
-//        listItems = listItems.map((item) => (
-//           <div key={item.id}>
-//              <label>
-//                 <input ref={this.state.checkRef} type= "checkbox" onChange={() => this.pickedItem(item.id)} defaultChecked={item.selected}></input>
-//                 <span>{item.description}</span>
-//              </label>
-//           </div>
-//          )) 
-//        return ( 
-//        <>
-//           <h1>Only The Basics -- Grocery List</h1>
-//           <h3>Mark What You Don't Need</h3>
-//              <label>
-//                 <input ref={this.state.filterRef} type= "checkbox" onChange={() => this.setState((prev) => ({filtered: !prev.filtered}))} defaultChecked={false}></input>
-//                 View Your Cart
-//              </label>
-//                 <hr></hr>
-//           <div style={{padding: "5px"}}>
-//              {listItems}
-//           </div>
-//                 <hr></hr>
-//        </>
-//        )
-//     }
-//  }
+//https://www.educative.io/blog/react-hooks-tutorial-todo-list
 
 import React from 'react'
 
@@ -153,7 +61,7 @@ let toDoList = []
              <h1>Workout To Do List</h1>
                 <label>
                    <input class="listCheckbox" ref = {this.state.filterRef} type = "checkbox" onChange={() => this.setState({filtered: !this.state.filtered})} defaultChecked={false}></input>
-                   Show uncompleted items
+                   Show incomplete items
                 </label>
              <div>
                <hr></hr>
